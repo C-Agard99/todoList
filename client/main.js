239fdb8d04865b239036839fc47cd4fcd0db76eb
ChanelAgard <chanel.agard99@gmail.com>
2021-07-14 19:54:56 -0400

    added sample code and user login

---
+++ client/main.js
@@ -0,0 +1,9 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import './main.html';
+
+
